c6ad6cd3e25ec39e630ac9383e13b22655c778be
gefu <George.Fu@mlb.com>
2016-02-29 17:23:30 -0500

    react repeater

--- src/main.js
+++ src/main.js
@@ -1,12 +1,40 @@
-//var React = require('react');
-//var ReactDOM = require('react-dom');
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+var Jsx = <jsx></jsx>;
+
+var ListItem = React.createClass({
+    render: function() {
+        return <li>n --> {this.props.index}</li>
+    }
+});
+
+var List = React.createClass({
+    render: function() {
+        var rows = this.props.rows.map(function(n) {
+            return <ListItem index={n} />;
+        });
+        return <ul>
+            {rows}
+        </ul>
+    }
+});
+
 ReactDOM.render(
-    React.createElement('h1', null, 'Hello, world!'),
+    <List rows={[1,2,3,4,5]} />,
     document.getElementById('main')
 );
+
+ReactDOM.render(
+    <ListItem index={"hi"} />,
+    document.getElementById('alt')
+);
+
+window.__react_test = function(rows) {
+    ReactDOM.render(
+        <List rows={rows} />,
+        document.getElementById('main')
+    );
+};
 
 console.log('hmm...');
